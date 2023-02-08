f08f416c9a0100f8225f2c0a7fdcc3051edca54d
Jmacias18 <maciascecenajoseantonio18@gmail.com>
2023-02-07 18:19:20 -0700

    yap

--- vue/ServicioApi/Servicio.js
+++ vue/ServicioApi/Servicio.js
@@ -1,19 +1,20 @@
-const app =Vue.createApp({
-    data(){
-        return{
-            amiibos: [],
-            amiiboss:[]
-        }
-    },methods: {
-        async obtener(){
-           const data= await fetch("https://amiiboapi.com/api/amiibo/",{
-            method: "GET"
-           });
-
-           const amiibos = await data.json();
-           this.amiibos= amiibos.amiibo;
-           console.log(this.amiibos);
+Vue.createApp({
+    data() {
+        return {
+          amiibos:[]
         }
     },
-   
-    })
+    methods:{
+        async obtener(){ // async crea la funcion asincrona y necesita del await para funcionar
+           const data = await fetch('https://amiiboapi.com/api/amiibo/',{
+                method: 'GET'
+           });
+           const amibo = await data.json(); //Usar await para sacar de la promesa la peticion. 
+           //No deja que la pagina se quede congelada
+           this.amiibos = amibo.amiibo;
+
+           console.log(this.amiibos)
+        }
+    }
+    }
+).mount("#app")
