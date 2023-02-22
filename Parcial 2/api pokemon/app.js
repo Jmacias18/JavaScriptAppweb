29018bef87627d6cbe27615aaceb7adcd91e8a5a
Jmacias18 <maciascecenajoseantonio18@gmail.com>
2023-02-22 14:37:12 -0700

    yap

--- Parcial 2/api pokemon/app.js
+++ Parcial 2/api pokemon/app.js
@@ -1,23 +1,34 @@
 Vue.createApp({
-    data(){
-        return{
-         pokemon:null
+    data() {
+        return {
+            PokemonEs:[],
+            loading:null,
+            cantidad:0
         }
     },
-    methods:{
-      async  getPokemon(){ //funcion asincrona el async ocupa el await
-            //GET   OBTENER INFORMACION
-            //POST  PARA CREAR RECURSOS
-            //PUT   MODIFICAR RECURSOS
-            //DELETE  ELIMINAR RECURSOS
-            const random=Math.floor(Math.random() * 200);
-          const response = await fetch("https://pokeapi.co/api/v2/pokemon/"+random, {
-                method:"GET"
+    mounted(){
+        this.getPokemon();
+    },
+     methods: {
+        /*  tipo de peticiones
+         Get ---- obtiene informacion
+         Post --- crear recursos
+         Put ---- modificar recursos
+         Delete --- elimminar recursos */
+        async getPokemon() {
+            this.loading=true
+            for(i=1;i<=this.cantidad;i++){
+/*             const random = Math.floor(Math.random() * 630);
+ */            const response = await fetch('https://pokeapi.co/api/v2/pokemon/'+ i, {
+                method: 'GET'
             });
-            const data= await response.json();
-            this.pokemon=data;
+            const data = await response.json();
+            this.Pokemon=data;
+            this.PokemonEs.push(this.Pokemon);
+        }
+        this.loading=false
         }
     }
-   
 
-    }).mount("#app")
+}
+).mount("#API")
