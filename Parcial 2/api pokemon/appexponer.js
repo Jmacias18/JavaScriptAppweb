3edcdbdd53c23bc056312a905a284c0263487661
Jmacias18 <maciascecenajoseantonio18@gmail.com>
2023-02-28 08:55:33 -0600

    ya

---
+++ Parcial 2/api pokemon/appexponer.js
@@ -0,0 +1,34 @@
+Vue.createApp({
+    data() {
+        return {
+            PokemonEs:[],
+            loading:null,
+            cantidad:0
+        }
+    },
+    mounted(){
+        this.getPokemon();
+    },
+     methods: {
+        /*  tipo de peticiones
+         Get ---- obtiene informacion
+         Post --- crear recursos
+         Put ---- modificar recursos
+         Delete --- elimminar recursos */
+        async getPokemon() {
+            this.loading=true
+            for(i=1;i<=this.cantidad;i++){
+/*             const random = Math.floor(Math.random() * 630);
+ */            const response = await fetch('https://randomuser.me/api/'+ i, {
+                method: 'GET'
+            });
+            const data = await response.json();
+            this.Pokemon=data;
+            this.PokemonEs.push(this.Pokemon);
+        }
+        this.loading=false
+        }
+    }
+
+}
+).mount("#API")
